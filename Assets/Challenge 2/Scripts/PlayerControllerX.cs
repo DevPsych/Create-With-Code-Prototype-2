a033ef0453c24399639da8b021e88f3640d3e0ad
DevPsych <140076838+DevPsych@users.noreply.github.com>
2023-07-27 14:51:27 +0900

    initial commit
    Challenge 2
    - press space to send dog
    - cooldown added to space action
    - random balls spawn
    - spawn time is varied
    Prototype 2
    - Not finished version

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float cooldownTime = 1.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Lower cooldown time if it is greater than -0.1
+        if (cooldownTime > -0.1)
+        {
+            cooldownTime -= Time.deltaTime;
+        }
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && cooldownTime < 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            Debug.Log(cooldownTime);
+            cooldownTime = 1.5f;
+        } else if (Input.GetKeyDown(KeyCode.Space) && cooldownTime > 0)
+        {
+            Debug.Log("Can use ability in " + cooldownTime + " seconds.");
+        }
+    }
+}
